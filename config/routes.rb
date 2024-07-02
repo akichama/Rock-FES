9bc4032f399369082aa9b8a19a1a3219f1c11315
akichama <aki0shir1_7413@outlook.jp>
2024-07-02 09:28:15 +0000

    top root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root controller: :homes, action: :top
+  get :about, controller: :homes, action: :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
